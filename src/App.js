9fa36116bf2ef6f70da0d8e057cff8e1507238fb
Werefox <jack@wrightus.com>
2022-06-19 16:53:40 -0400

    Added comments'

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import TopBar from './components/TopBar';
 import Banner from './components/Banner';
 import Stores from './components/Stores';
 
+// this exercise was a pain for some reason
+// css is not my favorite
+
 function App() {
 	return (
 		<div className="App">
